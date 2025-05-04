7a2c5b54a3ae68bc1de7ebcb2f6e2e6515b84670
parker-reed <Parker@ParkerHReed.com>
2025-05-03 21:13:14 -0700

    Added ParkerHReed link in Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ function App() {
 	return (
 		<div className='App'>
 			<AspectRatioCalculator />
+			<div className='footer'>
+				Designed & built by{" "}
+				<a href='https://parkerhreed.com/' alt='Parker H Reed Product Designer'>
+					Parker Reed
+				</a>
+				<div className='wolf'>
+					<img
+						src='https://res.cloudinary.com/drauw0cls/image/upload/v1746331308/wolf_d8gffo.svg'
+						alt='Parker Reed Product Designer'
+					/>
+				</div>
+			</div>
 		</div>
 	);
 }
